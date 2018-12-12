5cf25cb5d725472ef3eb7e81f826b89f329cab47
Hristiyan Trifonov <hristiyantrifonov@Hristiyans-iMac.local>
2018-12-12 15:27:51 +0000

    Add Message entity resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_150353) do
+ActiveRecord::Schema.define(version: 2018_12_12_152354) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
